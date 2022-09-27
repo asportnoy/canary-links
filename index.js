3703f161aa0fb6559b3ccb06016ddf335e86267d
Albert Portnoy <albert@albertp.dev>
2022-09-26 23:39:03 -0500

    Fix context menu

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ module.exports = class ViewRaw extends Plugin {
 			checkChildren(res, url);
 
 			return res;
-		}
+		};
 
 		injectContextMenu(
 			'copy-link-contextmenu',
@@ -100,7 +100,7 @@ module.exports = class ViewRaw extends Plugin {
 		);
 
 		inject('copy-link-dotmenu', MessageMenuItems, 'copyLink', args => {
-			copy(getURL(args[0], args[1]));
+			copy(getMessageURL(args[0], args[1]));
 		});
 	}
 };
