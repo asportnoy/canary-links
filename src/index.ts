5b57d12d9776e289108143b0e372d3c10702222a
Albert Portnoy <albert@albertp.dev>
2022-12-13 21:34:29 -0600

    Make it work!

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,17 @@
-import { Injector, webpack } from "replugged";
-import { AnyFunction } from "replugged/dist/types/util";
+import { webpack } from "replugged";
 
-const inject = new Injector();
-
-export async function start(): Promise<void> {
-  const typingMod = (await webpack.waitForModule(webpack.filters.byProps("startTyping"))) as {
-    startTyping: AnyFunction;
-  };
-  const getChannelMod = (await webpack.waitForModule(webpack.filters.byProps("getChannel"))) as {
-    getChannel: (id: string) => {
-      name: string;
-    };
-  };
-
-  if (typingMod && getChannelMod) {
-    inject.instead(typingMod, "startTyping", ([channel]) => {
-      const channelObj = getChannelMod.getChannel(channel as string);
-      console.log(`Typing prevented! Channel: #${channelObj?.name ?? "unknown"} (${channel}).`);
-    });
-  }
-}
-
-export function stop(): void {
-  inject.uninjectAll();
+export function runPlaintextPatches(): void {
+  webpack.patchPlaintext([
+    {
+      replacements: [
+        {
+          match: /(""\.concat\(location\.protocol,"\/\/"\)\.concat\()location\.host(\)\.concat\(\w+\.Z5c\.CHANNEL\(.+?\)\))/g,
+          replace: (_, prefix, suffix) => {
+            console.log(_, prefix, suffix);
+            return `${prefix}"discord.com"${suffix}`;
+          },
+        },
+      ],
+    },
+  ]);
 }
